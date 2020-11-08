4d31613262e53eb76bfd6afde7e4419a04d494f5
thilo <thiloschmid@bluewin.ch>
2020-11-08 17:53:22 +0100

    added heroku postgresql database

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -2,11 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./users.db'
+# 1. sqlite
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./users.db' 
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
+# )
+
+# 2. postgreSQL (needed for heroku)
+
+POSTGRES_SQLALCHEMY_DATABASE_URL = 'postgres://vfpfzjftdmktgp:af199191fa0e5b30ddd4eee96a62cfb1376d2d7f6bfbbb2ab10779c184b00af8@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/dfoj4s9b2uqg4n'
+engine = create_engine(POSTGRES_SQLALCHEMY_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

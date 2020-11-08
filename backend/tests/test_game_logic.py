d71327fac211598ad6f4e856f2aac88d04b35a4f
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-11-08 11:28:02 +0100

    adapt tests

--- backend/tests/test_game_logic.py
+++ backend/tests/test_game_logic.py
@@ -2,41 +2,41 @@ import sys
 
 
 from app.game_logic.brandi import Brandi
-from app.game_logic.user import User
+from app.game_logic.player import Player
 
-from app.game_logic.field import EntryExitNode
+from app.game_logic.field import GameNode
 
 
 class TestGame():
     def setup_class(self):
-        self.id1 = User('Bene', 0)
-        self.id2 = User('Lara', 1)
-        self.id3 = User('Thilo', 2)
-        self.id4 = User('Bibi', 3)
-        self.game = Brandi('ABCD', seed=1, host=self.id1)
+        self.player_1 = Player('bene', 'Bene', 0)
+        self.player_2 = Player('lara', 'Lara', 1)
+        self.player_3 = Player('thilo', 'Thilo', 2)
+        self.player_4 = Player('bibi', 'Bibi', 3)
+        self.game = Brandi('ABCD', seed=1, host=self.player_1)
 
     def test_pre_start_game(self):
         assert self.game.game_state == 0
-        assert self.game.players[0].username == 'Bene'
+        assert self.game.players['bene'].username == 'Bene'
         assert len(self.game.players.values()) == 1
 
     def test_player_join(self):
-        self.game.player_join(self.id2)
-        self.game.player_join(self.id3)
-        self.game.player_join(self.id4)
+        self.game.player_join(self.player_2)
+        self.game.player_join(self.player_3)
+        self.game.player_join(self.player_4)
 
-        assert len(self.game.order) == 4
-        assert self.game.players[1].username == 'Lara'
+        assert len(self.game.players.values()) == 4
+        assert self.game.players['lara'].username == 'Lara'
 
     def test_change_teams(self):
-        self.game.change_teams([self.id1, self.id3, self.id2, self.id4])
+        self.game.change_postion(self.player_1, 2)
 
-        assert self.game.players[1].username == 'Lara'
-        assert self.game.players[2].username == 'Thilo'
-        assert self.game.order == [0, 2, 1, 3]
+        assert self.game.players['thilo'].position == 0
+        assert self.game.players['lara'].position == 1
+        assert self.game.players['bene'].position == 2
 
     def test_start_game(self):
         self.game.start_game()
 
-        assert hasattr(self.game.players[0], 'starting_node')
-        assert isinstance(self.game.players[0].starting_node, EntryExitNode)
+        assert hasattr(self.game.players['bene'], 'starting_node')
+        assert isinstance(self.game.players['bene'].starting_node, GameNode)

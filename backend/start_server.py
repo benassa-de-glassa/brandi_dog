42915e7dbc211acf9e7c5d124a280db1ebf1ea93
thilo <thiloschmid@bluewin.ch>
2020-11-14 13:03:54 +0100

    read log-level from environment variable if possible

--- backend/start_server.py
+++ backend/start_server.py
@@ -1,10 +1,13 @@
+import os
+
 import uvicorn
+
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
 
 if __name__ == "__main__":
     uvicorn.run(
         'app.main:sio_app', 
         host="0.0.0.0", 
         port=8000, 
-        reload=True,
-        log_level='info'
+        reload=True
     )

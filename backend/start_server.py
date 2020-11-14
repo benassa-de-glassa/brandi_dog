17832ac597daf47eb210341d232033f338bc6a3f
thilo <thiloschmid@bluewin.ch>
2020-11-14 21:50:56 +0100

    try to load host and port from env variable

--- backend/start_server.py
+++ backend/start_server.py
@@ -6,10 +6,13 @@ from dotenv import load_dotenv
 
 load_dotenv()   # (try to) load environment variables from .env
 
+HOST = os.environ.get('HOST', '0.0.0.0')
+PORT = os.environ.get('PORT', 8000)
+
 if __name__ == "__main__":
     uvicorn.run(
-        'app.main:sio_app', 
-        host="0.0.0.0", 
-        port=8000, 
+        'app.main:sio_app',
+        host=HOST, 
+        port=PORT, 
         reload=True
     )

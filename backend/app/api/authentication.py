b52dd95f7e6dee3919e93f8c4b9a08c91000e500
thilo <thiloschmid@bluewin.ch>
2020-11-14 16:34:04 +0100

    w i p

--- backend/app/api/authentication.py
+++ backend/app/api/authentication.py
@@ -20,6 +20,7 @@ from app import models
 
 # TODO: ???
 from app.models.token import Token # no idea why this is required ???
+# import app.models
 
 from app.game_logic.user import User
 from app.database import crud, db_models

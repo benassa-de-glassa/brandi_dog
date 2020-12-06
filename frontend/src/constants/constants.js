721d87076e596a29c832f9eb813c642aad951ba1
thilo <thiloschmid@bluewin.ch>
2020-12-06 16:11:38 +0100

    made it read heroku environment variables in 'production', still use local definitions in 'deployment' and 'test' environments

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,3 +1,6 @@
-export const API_URL = "https://boomer-backend.herokuapp.com/v1/"
+// Export the API and socket URLs. The environment variable NODE_ENV is
+// automatically set to 'production', 'test', or 'deployment'. For 'production' 
+// the URLs are read from environment variables which are added on heroku. 
 
-export const SIO_URL = "https://boomer-backend.herokuapp.com"
+export const API_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : "http://localhost:8000/v1/"
+export const SIO_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_SIO_URL : "http://localhost:8000"
--- frontend/src/constants/game_config.js
+++ frontend/src/constants/game_config.js
@@ -1,6 +1,3 @@
-// export const DEBUG = true
-export const DEBUG = false
-
 export const possibleActions = {
     'A': [0, 1, 11],
     '2': [2],

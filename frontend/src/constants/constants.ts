cd00054526f4d2db3a4a4f37469859dc21e931b0
Thilo Schmid <thiloschmid@bluewin.ch>
2020-12-29 00:20:01 +0100

    read API and SIO URL from environment variable in 'production' environment

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,10 +1,12 @@
 import { Action } from "../models/action.model";
 import { CardKey } from "../models/card.model";
 
-// defaults to run locally, in case the environment variables don't specify
-// different URLs
-export const API_URL: string = "http://localhost:8000/v1/";
-export const SIO_URL: string = "http://localhost:8000";
+// Export the API and socket URLs. The environment variable NODE_ENV is
+// automatically set to 'production', 'test', or 'deployment'. For 'production' 
+// the URLs are read from environment variables which are added on heroku. 
+
+export const API_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : "http://localhost:8000/v1/"
+export const SIO_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_SIO_URL : "http://localhost:8000" 
 
 export const possibleActions: { [key in CardKey]: Action[] } = {
   A: [0, 1, 11],
